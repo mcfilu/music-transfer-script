5645982bafaf3b1b470ca81466c4bdacc868bdf2
mcfilu <filip.1olszewski@gmail.com>
2024-03-03 21:45:13 +0000

    updating the requirements.txt plus adding the argument to main.py requiring the playlist url to be passed

--- main.py
+++ main.py
@@ -5,10 +5,11 @@ from youtube.download import download_songs
 from drive.auth import authenticate
 from drive.upload import upload_folder
 from helpers.clear import delete_downloads
+import sys
 
 
-def main():
-    playlist_url = "https://open.spotify.com/playlist/2odeDPVVEP0MEvwqpTnMLN"
+def main(playlist_url):
+    # playlist_url = "https://open.spotify.com/playlist/2odeDPVVEP0MEvwqpTnMLN"
     spotify_id, spotify_secret, drive_key = load_credentials()
     token = auth_user(client_id=spotify_id, client_secret=spotify_secret)
     spotipy_ins = create_instance(token)
@@ -21,4 +22,8 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        parameter = sys.argv[1]
+    except Exception:
+        raise KeyError("Application requires one argument: playlist url passed as a string")
+    main(parameter)
